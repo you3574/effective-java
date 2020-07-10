bbc13ae96004cfe95ccc0ec8b1aa86ca275f7f4a
DESKTOP-CPF5K67\yoobin <you3574@naver.com>
2020-07-10 17:47:03 +0900

    effective_java item55~57

--- effective_java/src/item18/BigRibBurger.java
+++ effective_java/src/item18/BigRibBurger.java
@@ -1,5 +1,29 @@
 package item18;
 
+/**
+* 롯데리아에서 파는 맛있는 BigRib버거
+*/
 public class BigRibBurger {
-
+             private int price;
+             private String description;
+ 
+             public BigRibBurger(int price,String description) {
+                           this.price = price;
+                           this.description = description;
+             }
+             public BigRibBurger(int price) {
+                           this(price,"best selling burger in 2002");
+             }
+             public BigRibBurger(String description) {
+                           this(2500,description);
+             }
+             public BigRibBurger() {
+                           this(2500,"best selling burger in 2002");
+             }
+             public int getPrice() {
+                           return price;
+             }
+             public String getDescription() {
+                           return description;
+             }
 }
